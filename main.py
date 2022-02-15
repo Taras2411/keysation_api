4c6fd6f1bbaec70137da8be66c2880498d90d839
Chel_KUBUNTA <taraspushkar24@gmail.com>
2022-02-15 09:33:41 +0200

    added mod_user to api

--- main.py
+++ main.py
@@ -23,6 +23,41 @@ mydb = mysql.connector.connect(
     database="key_station")
 mycursor = mydb.cursor()
     
+@app.get("/api/users/")
+def users_get():
+    mycursor.execute(f'SELECT * FROM key_station.Teachers;')
+    restuple = mycursor.fetchall()
+    json_dict = {}
+    for z in restuple:
+        json_dict[z[0]] = ({"id": z[0],"FIO": z[1],"en":z[2]})
+    json_compatible_item_data = jsonable_encoder(json_dict)
+    print(json_compatible_item_data)
+    return JSONResponse(content=json_compatible_item_data)
+    
+@app.get("/api/users/{getuser_id}")
+def user_get(getuser_id):
+    mycursor.execute(f'SELECT * FROM key_station.Teachers WHERE id = "{getuser_id}" ;')
+    restuple = mycursor.fetchall()
+    json_dict = {}
+    for z in restuple:
+        json_dict[z[0]] = ({"id": z[0],"FIO": z[1],"en":z[2]})
+    json_compatible_item_data = jsonable_encoder(json_dict)
+    print(json_compatible_item_data)
+    return JSONResponse(content=json_compatible_item_data)
+    
+    
+    
+@app.get("/api/keys/")
+def keys_get():
+    
+    mycursor.execute(f'SELECT id,Name,en,Comment FROM key_station.Keys')
+    restuple = mycursor.fetchall()
+    json_dict = {}
+    for z in restuple:
+        json_dict[z[0]] = ({"id": z[0],"real_name": z[1],"en":z[2],"comment": z[3]})
+    json_compatible_item_data = jsonable_encoder(json_dict)
+    print(json_compatible_item_data)
+    return JSONResponse(content=json_compatible_item_data)
 
 @app.get("/api/keys/{key_id}")
 def key_get(key_id):
@@ -48,6 +83,22 @@ def userDelPost(uname:UserName):
     mycursor.execute(f'INSERT INTO key_station.Teachers (FIO,en) VALUES ("{temp}",1);')
     mydb.commit()
     return uname.name
+
+class User(BaseModel):
+    user_id: int 
+    FIO: str 
+    ena: int
+
+@app.post("/api/users/mod_user")
+def userModPost(fullUserInfo: User):
+    print(fullUserInfo)
+    temp = f"UPDATE key_station.Teachers SET FIO='{fullUserInfo.FIO}' WHERE id={fullUserInfo.user_id};"
+    print(temp)
+    mycursor.execute(temp)
+    mydb.commit()
+    #return fullUserInfo
+
+
 
 class UserId(BaseModel):
     user_id:int
